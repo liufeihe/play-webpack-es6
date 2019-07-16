8ced468d85ac49b10bdb34d12c2e8502c64b0810
liufeihe <lfhe2014@163.com>
2019-07-16 18:59:19 +0800

    add devSever

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin'); 
 
 const getEntries = ()=>{
     return {
@@ -8,8 +9,14 @@ const getEntries = ()=>{
     }
 }
 
-module.exports = {
+let webpackCfg = {
     entry: getEntries(),
+    devServer: {
+        index: 'a.html',
+        contentBase: './dist',
+        // port: 8080,
+        open: true
+    },
     output: {
         path: path.resolve(__dirname, '../dist'),
         filename: 'js/[name]-[chunkhash].js'
@@ -24,3 +31,38 @@ module.exports = {
     },
     plugins: []
 }
+
+//生存多个页面
+var pages = [
+    {
+        filename: 'a.html',
+        template: 'src/pages/a/a.html',
+        chunks: 'a'
+    },
+    {
+        filename: 'b.html',
+        template: 'src/pages/b/b.html',
+        chunks: 'b'
+    },
+    {
+        filename: 'c.html',
+        template: 'src/pages/c/c.html',
+        chunks: 'c'
+    }
+]
+pages.forEach(function(page) {
+    var conf = {
+        template: page.template, // html模板路径
+        filename: page.filename, // 生成的html存放路径,文件名，相对于path
+        chunks: [page.chunks],
+        inject: 'body', // //js插入的位置
+        hash: false,
+        minify: { // 压缩HTML文件
+            removeComments: true, // 移除HTML中的注释
+            collapseWhitespace: false, // 删除空白符与换行符
+            removeAttributeQuotes: true
+        },
+    }
+    webpackCfg.plugins.push(new htmlWebpackPlugin(conf))
+});
+module.exports = webpackCfg;
